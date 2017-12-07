6f5cd983e52d1a888a9fb4f90a857a7923349f85
poskart <piotrpos93@gmail.com>
2017-12-07 23:43:03 +0100

    Initial commit with CpuMonitor class

---
+++ src/library_src/CpuMonitor.cpp
@@ -0,0 +1,20 @@
+/*
+ * CpuMonitor.cpp
+ *
+ *  Created on: Dec 7, 2017
+ *      Author: piotr
+ */
+
+#include "CpuMonitor.h"
+
+CpuMonitor::CpuMonitor()
+{
+	// TODO Auto-generated constructor stub
+
+}
+
+CpuMonitor::~CpuMonitor()
+{
+	// TODO Auto-generated destructor stub
+}
+
---
+++ tests_src/hello_test/CpuMonitor_test.cpp
@@ -0,0 +1,9 @@
+/*
+ * CpuMonitor_test.cpp
+ *
+ *  Created on: Dec 7, 2017
+ *      Author: piotr
+ */
+
+#include "CpuMonitor.h"
+
