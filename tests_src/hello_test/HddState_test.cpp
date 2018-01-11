0bb3ec215d5db9facef28762a938a6cbc4ebe988
poskart <piotrpos93@gmail.com>
2018-01-11 20:02:47 +0100

    Test cases for state getters

--- tests_src/hello_test/HddState_test.cpp
+++
@@ -1,9 +0,0 @@
-/*
- * HddState_test.cpp
- *
- *  Created on: Dec 8, 2017
- *      Author: piotr
- */
-
-#include "HddState.hpp"
-
--- tests_src/hello_test/RamState_test.cpp
+++
@@ -1,9 +0,0 @@
-/*
- * RamState_test.cpp
- *
- *  Created on: Dec 8, 2017
- *      Author: piotr
- */
-
-#include "RamState.hpp"
-
