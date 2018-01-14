6de0d22895f0b966268e4a46526215ea7bc6ae3c
poskart <piotrpos93@gmail.com>
2018-01-14 10:42:00 +0100

    Server http instead of https, CMakeList updated

--- src/https_examples.cpp
+++ src/https_examples.cpp
@@ -1,5 +1,5 @@
-#include "client_https.hpp"
-#include "server_https.hpp"
+#include "client_http.hpp"
+#include "server_http.hpp"
 #include "smtp_client.hpp"
 
 // Added for the json-example
@@ -20,215 +20,224 @@ using namespace std;
 // Added for the json-example:
 using namespace boost::property_tree;
 
-using HttpsServer = SimpleWeb::Server<SimpleWeb::HTTPS>;
-using HttpsClient = SimpleWeb::Client<SimpleWeb::HTTPS>;
+using HttpServer = SimpleWeb::Server<SimpleWeb::HTTP>;
+using HttpClient = SimpleWeb::Client<SimpleWeb::HTTP>;
 
 void exampleHttpsServerExecution() {
-    // HTTPS-server at port 8080 using 1 thread
-    // Unless you do more heavy non-threaded processing in the resources,
-    // 1 thread is usually faster than several threads
-    HttpsServer server("../cert.pem", "../key.pem");
-    server.config.port = 8080;
+	// HTTP-server at port 8080 using 1 thread
+	  // Unless you do more heavy non-threaded processing in the resources,
+	  // 1 thread is usually faster than several threads
+	  HttpServer server;
+	  server.config.port = 8080;
 
-    // Add resources using path-regex and method-string, and an anonymous function
-    // POST-example for the path /string, responds the posted string
-    server.resource["^/string$"]["POST"] =
-            [](shared_ptr<HttpsServer::Response> response, shared_ptr<HttpsServer::Request> request) {
-                // Retrieve string:
-                auto content = request->content.string();
-                // request->content.string() is a convenience function for:
-                // stringstream ss;
-                // ss << request->content.rdbuf();
-                // auto content=ss.str();
+	  // Add resources using path-regex and method-string, and an anonymous function
+	  // POST-example for the path /string, responds the posted string
+	  server.resource["^/string$"]["POST"] = [](shared_ptr<HttpServer::Response> response, shared_ptr<HttpServer::Request> request) {
+	    // Retrieve string:
+	    auto content = request->content.string();
+	    // request->content.string() is a convenience function for:
+	    // stringstream ss;
+	    // ss << request->content.rdbuf();
+	    // auto content=ss.str();
 
-                *response << "HTTP/1.1 200 OK\r\nContent-Length: " << content.length() << "\r\n\r\n"
-                          << content;
+	    *response << "HTTP/1.1 200 OK\r\nContent-Length: " << content.length() << "\r\n\r\n"
+	              << content;
 
-                // Alternatively, use one of the convenience functions, for instance:
-                // response->write(content);
-            };
 
-    // POST-example for the path /json, responds firstName+" "+lastName from the posted json
-    // Responds with an appropriate error message if the posted json is not valid, or if firstName or lastName is missing
-    // Example posted json:
-    // {
-    //   "firstName": "John",
-    //   "lastName": "Smith",
-    //   "age": 25
-    // }
-    server.resource["^/json$"]["POST"] =
-            [](shared_ptr<HttpsServer::Response> response, shared_ptr<HttpsServer::Request> request) {
-                try {
-                    ptree pt;
-                    read_json(request->content, pt);
+	    // Alternatively, use one of the convenience functions, for instance:
+	    // response->write(content);
+	  };
 
-                    auto name = pt.get<string>("firstName") + " " + pt.get<string>("lastName");
+	  // POST-example for the path /json, responds firstName+" "+lastName from the posted json
+	  // Responds with an appropriate error message if the posted json is not valid, or if firstName or lastName is missing
+	  // Example posted json:
+	  // {
+	  //   "firstName": "John",
+	  //   "lastName": "Smith",
+	  //   "age": 25
+	  // }
+	  server.resource["^/json$"]["POST"] = [](shared_ptr<HttpServer::Response> response, shared_ptr<HttpServer::Request> request) {
+	    try {
+	      ptree pt;
+	      read_json(request->content, pt);
 
-                    *response << "HTTP/1.1 200 OK\r\n"
-                              << "Content-Length: " << name.length() << "\r\n\r\n"
-                              << name;
-                }
-                catch (const exception &e) {
-                    *response << "HTTP/1.1 400 Bad Request\r\nContent-Length: " << strlen(e.what()) << "\r\n\r\n"
-                              << e.what();
-                }
+	      auto name = pt.get<string>("firstName") + " " + pt.get<string>("lastName");
 
-                // Alternatively, using a convenience function:
-                // try {
-                //     ptree pt;
-                //     read_json(request->content, pt);
+	      *response << "HTTP/1.1 200 OK\r\n"
+	                << "Content-Length: " << name.length() << "\r\n\r\n"
+	                << name;
+	    }
+	    catch(const exception &e) {
+	      *response << "HTTP/1.1 400 Bad Request\r\nContent-Length: " << strlen(e.what()) << "\r\n\r\n"
+	                << e.what();
+	    }
 
-                //     auto name=pt.get<string>("firstName")+" "+pt.get<string>("lastName");
-                //     response->write(name);
-                // }
-                // catch(const exception &e) {
-                //     response->write(SimpleWeb::StatusCode::client_error_bad_request, e.what());
-                // }
-            };
 
-    // GET-example for the path /info
-    // Responds with request-information
-    server.resource["^/info$"]["GET"] =
-            [](shared_ptr<HttpsServer::Response> response, shared_ptr<HttpsServer::Request> request) {
-                stringstream stream;
-                stream << "<h1>Request from " << request->remote_endpoint_address() << ":"
-                       << request->remote_endpoint_port() << "</h1>";
+	    // Alternatively, using a convenience function:
+	    // try {
+	    //     ptree pt;
+	    //     read_json(request->content, pt);
 
-                stream << request->method << " " << request->path << " HTTP/" << request->http_version;
+	    //     auto name=pt.get<string>("firstName")+" "+pt.get<string>("lastName");
+	    //     response->write(name);
+	    // }
+	    // catch(const exception &e) {
+	    //     response->write(SimpleWeb::StatusCode::client_error_bad_request, e.what());
+	    // }
+	  };
 
-                stream << "<h2>Query Fields</h2>";
-                auto query_fields = request->parse_query_string();
-                for (auto &field : query_fields)
-                    stream << field.first << ": " << field.second << "<br>";
+	  // GET-example for the path /info
+	  // Responds with request-information
+	  server.resource["^/info$"]["GET"] = [](shared_ptr<HttpServer::Response> response, shared_ptr<HttpServer::Request> request) {
+	    stringstream stream;
+	    stream << "<h1>Request from " << request->remote_endpoint_address() << ":" << request->remote_endpoint_port() << "</h1>";
 
-                stream << "<h2>Header Fields</h2>";
-                for (auto &field : request->header)
-                    stream << field.first << ": " << field.second << "<br>";
+	    stream << request->method << " " << request->path << " HTTP/" << request->http_version;
 
-                response->write(stream);
-            };
+	    stream << "<h2>Query Fields</h2>";
+	    auto query_fields = request->parse_query_string();
+	    for(auto &field : query_fields)
+	      stream << field.first << ": " << field.second << "<br>";
 
-    // GET-example for the path /match/[number], responds with the matched string in path (number)
-    // For instance a request GET /match/123 will receive: 123
-    server.resource["^/match/([0-9]+)$"]["GET"] =
-            [](shared_ptr<HttpsServer::Response> response, shared_ptr<HttpsServer::Request> request) {
-                response->write(request->path_match[1]);
-            };
+	    stream << "<h2>Header Fields</h2>";
+	    for(auto &field : request->header)
+	      stream << field.first << ": " << field.second << "<br>";
 
-    // GET-example simulating heavy work in a separate thread
-    server.resource["^/work$"]["GET"] =
-            [](shared_ptr<HttpsServer::Response> response, shared_ptr<HttpsServer::Request> /*request*/) {
-                thread work_thread([response] {
-                    this_thread::sleep_for(chrono::seconds(5));
-                    response->write("Work done");
-                });
-                work_thread.detach();
-            };
+	    response->write(stream);
+	  };
 
-    // Default GET-example. If no other matches, this anonymous function will be called.
-    // Will respond with content in the web/-directory, and its subdirectories.
-    // Default file: index.html
-    // Can for instance be used to retrieve an HTML 5 client that uses REST-resources on this server
-    /*
-    server.default_resource["GET"] =
-            [](shared_ptr<HttpsServer::Response> response, shared_ptr<HttpsServer::Request> request) {
-                try {
-                    auto web_root_path = boost::filesystem::canonical("web");
-                    auto path = boost::filesystem::canonical(web_root_path / request->path);
-                    // Check if path is within web_root_path
-                    if (distance(web_root_path.begin(), web_root_path.end()) > distance(path.begin(), path.end()) ||
-                        !equal(web_root_path.begin(), web_root_path.end(), path.begin()))
-                        throw invalid_argument("path must be within root path");
-                    if (boost::filesystem::is_directory(path))
-                        path /= "index.html";
+	  // GET-example for the path /match/[number], responds with the matched string in path (number)
+	  // For instance a request GET /match/123 will receive: 123
+	  server.resource["^/match/([0-9]+)$"]["GET"] = [](shared_ptr<HttpServer::Response> response, shared_ptr<HttpServer::Request> request) {
+	    response->write(request->path_match[1]);
+	  };
 
-                    SimpleWeb::CaseInsensitiveMultimap header;
+	  // GET-example simulating heavy work in a separate thread
+	  server.resource["^/work$"]["GET"] = [](shared_ptr<HttpServer::Response> response, shared_ptr<HttpServer::Request> /*request*/) {
+	    thread work_thread([response] {
+	      this_thread::sleep_for(chrono::seconds(5));
+	      response->write("Work done");
+	    });
+	    work_thread.detach();
+	  };
 
-                    // Uncomment the following line to enable Cache-Control
-                    // header.emplace("Cache-Control", "max-age=86400");
+	  // Default GET-example. If no other matches, this anonymous function will be called.
+	  // Will respond with content in the web/-directory, and its subdirectories.
+	  // Default file: index.html
+	  // Can for instance be used to retrieve an HTML 5 client that uses REST-resources on this server
+	  server.default_resource["GET"] = [](shared_ptr<HttpServer::Response> response, shared_ptr<HttpServer::Request> request) {
+	    try {
+	      auto web_root_path = boost::filesystem::canonical("web");
+	      auto path = boost::filesystem::canonical(web_root_path / request->path);
+	      // Check if path is within web_root_path
+	      if(distance(web_root_path.begin(), web_root_path.end()) > distance(path.begin(), path.end()) ||
+	         !equal(web_root_path.begin(), web_root_path.end(), path.begin()))
+	        throw invalid_argument("path must be within root path");
+	      if(boost::filesystem::is_directory(path))
+	        path /= "index.html";
 
-                    auto ifs = make_shared<ifstream>();
-                    ifs->open(path.string(), ifstream::in | ios::binary | ios::ate);
+	      SimpleWeb::CaseInsensitiveMultimap header;
 
-                    if (*ifs) {
-                        auto length = ifs->tellg();
-                        ifs->seekg(0, ios::beg);
+	      // Uncomment the following line to enable Cache-Control
+	      // header.emplace("Cache-Control", "max-age=86400");
 
-                        header.emplace("Content-Length", to_string(length));
-                        response->write(header);
+	#ifdef HAVE_OPENSSL
+	//    Uncomment the following lines to enable ETag
+	//    {
+	//      ifstream ifs(path.string(), ifstream::in | ios::binary);
+	//      if(ifs) {
+	//        auto hash = SimpleWeb::Crypto::to_hex_string(SimpleWeb::Crypto::md5(ifs));
+	//        header.emplace("ETag", "\"" + hash + "\"");
+	//        auto it = request->header.find("If-None-Match");
+	//        if(it != request->header.end()) {
+	//          if(!it->second.empty() && it->second.compare(1, hash.size(), hash) == 0) {
+	//            response->write(SimpleWeb::StatusCode::redirection_not_modified, header);
+	//            return;
+	//          }
+	//        }
+	//      }
+	//      else
+	//        throw invalid_argument("could not read file");
+	//    }
+	#endif
 
-                        // Trick to define a recursive function within this scope (for example purposes)
-                        class FileServer {
-                        public:
-                            static void read_and_send(const shared_ptr<HttpsServer::Response> &response,
-                                                      const shared_ptr<ifstream> &ifs) {
-                                // Read and send 128 KB at a time
-                                static vector<char> buffer(131072);// Safe when server is running on one thread
-                                streamsize read_length;
-                                if ((read_length = ifs->read(&buffer[0],
-                                                             static_cast<streamsize>(buffer.size())).gcount()) > 0) {
-                                    response->write(&buffer[0], read_length);
-                                    if (read_length == static_cast<streamsize>(buffer.size())) {
-                                        response->send([response, ifs](const SimpleWeb::error_code &ec) {
-                                            if (!ec)
-                                                read_and_send(response, ifs);
-                                            else
-                                                cerr << "Connection interrupted" << endl;
-                                        });
-                                    }
-                                }
-                            }
-                        };
-                        FileServer::read_and_send(response, ifs);
-                    } else
-                        throw invalid_argument("could not read file");
-                }
-                catch (const exception &e) {
-                    response->write(SimpleWeb::StatusCode::client_error_bad_request,
-                                    "Could not open path " + request->path + ": " + e.what());
-                }
-            };*/
+	      auto ifs = make_shared<ifstream>();
+	      ifs->open(path.string(), ifstream::in | ios::binary | ios::ate);
 
-    server.on_error =
-            [](shared_ptr<HttpsServer::Request> /*request*/, const SimpleWeb::error_code & /*ec*/) {
-                // Handle errors here
-            };
+	      if(*ifs) {
+	        auto length = ifs->tellg();
+	        ifs->seekg(0, ios::beg);
 
-    thread server_thread([&server]() {
-        // Start server
-        server.start();
-    });
+	        header.emplace("Content-Length", to_string(length));
+	        response->write(header);
 
-    // Wait for server to start so that the client can connect
-    this_thread::sleep_for(chrono::seconds(1));
+	        // Trick to define a recursive function within this scope (for example purposes)
+	        class FileServer {
+	        public:
+	          static void read_and_send(const shared_ptr<HttpServer::Response> &response, const shared_ptr<ifstream> &ifs) {
+	            // Read and send 128 KB at a time
+	            static vector<char> buffer(131072); // Safe when server is running on one thread
+	            streamsize read_length;
+	            if((read_length = ifs->read(&buffer[0], static_cast<streamsize>(buffer.size())).gcount()) > 0) {
+	              response->write(&buffer[0], read_length);
+	              if(read_length == static_cast<streamsize>(buffer.size())) {
+	                response->send([response, ifs](const SimpleWeb::error_code &ec) {
+	                  if(!ec)
+	                    read_and_send(response, ifs);
+	                  else
+	                    cerr << "Connection interrupted" << endl;
+	                });
+	              }
+	            }
+	          }
+	        };
+	        FileServer::read_and_send(response, ifs);
+	      }
+	      else
+	        throw invalid_argument("could not read file");
+	    }
+	    catch(const exception &e) {
+	      response->write(SimpleWeb::StatusCode::client_error_bad_request, "Could not open path " + request->path + ": " + e.what());
+	    }
+	  };
 
-    // Client examples
-    // Second create() parameter set to false: no certificate verification
-    HttpsClient client("localhost:8080", false);
+	  server.on_error = [](shared_ptr<HttpServer::Request> /*request*/, const SimpleWeb::error_code & /*ec*/) {
+	    // Handle errors here
+	  };
 
-    string json_string =
-            "{\"firstName\": \"John\",\"lastName\": \"Smith\",\"age\": 25}";
+	  thread server_thread([&server]() {
+	    // Start server
+	    server.start();
+	  });
 
-    // Synchronous request examples
-    try {
-        auto r1 = client.request("GET", "/match/123");
-        cout << r1->content.rdbuf() << endl; // Alternatively, use the convenience function r1->content.string()
+	  // Wait for server to start so that the client can connect
+	  this_thread::sleep_for(chrono::seconds(1));
 
-        auto r2 = client.request("POST", "/string", json_string);
-        cout << r2->content.rdbuf() << endl;
-    } catch (const SimpleWeb::system_error &e) {
-        cerr << "Client request error: " << e.what() << endl;
-    }
+	  // Client examples
+	  HttpClient client("localhost:8080");
 
-    // Asynchronous request example
-    client.request("POST", "/json", json_string,
-                   [](shared_ptr<HttpsClient::Response> response, const SimpleWeb::error_code &ec) {
-                       if (!ec)
-                           cout << response->content.rdbuf() << endl;
-                   });
-    client.io_service->run();
+	  string json_string = "{\"firstName\": \"John\",\"lastName\": \"Smith\",\"age\": 25}";
 
-    server_thread.join();
+	  // Synchronous request examples
+	  try {
+	    auto r1 = client.request("GET", "/match/123");
+	    cout << r1->content.rdbuf() << endl; // Alternatively, use the convenience function r1->content.string()
+
+	    auto r2 = client.request("POST", "/string", json_string);
+	    cout << r2->content.rdbuf() << endl;
+	  }
+	  catch(const SimpleWeb::system_error &e) {
+	    cerr << "Client request error: " << e.what() << endl;
+	  }
+
+	  // Asynchronous request example
+	  client.request("POST", "/json", json_string, [](shared_ptr<HttpClient::Response> response, const SimpleWeb::error_code &ec) {
+	    if(!ec)
+	      cout << response->content.rdbuf() << endl;
+	  });
+	  client.io_service->run();
+
+	server_thread.join();
 }
 
 int main() {
